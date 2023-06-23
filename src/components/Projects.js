26f38ee45938092efc3c405f575fc3a1fa66559a
carlosmejia10 <117192998+carlosmejia10@users.noreply.github.com>
2023-06-23 17:14:27 -0500

    Update Projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -51,7 +51,9 @@ export const Projects = () => {
               {({ isVisible }) =>
               <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
                 <h2>Projects</h2>
-                <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
+                <p>He trabajado en distintos proyectos: una pagina web comercial para un restaurante, paginas basicas de frontend para tiendas online; y algunas apps de backend, en mi ultimo proyecto realize una base de datos
+                  para la organizacion de datos de camaras trampa de deteccion de especies, ademas de esto se realizo una api que permite realizar la busqueda de las especies y datos especificos, ademas se conecto con un frontend, 
+                  dando la posibilidad de actualizar datos siendo administrador, y buscar datos siendo un usuario comun; Todo con datos de GBIF.</p>
                 <Tab.Container id="projects-tabs" defaultActiveKey="first">
                   <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
                     <Nav.Item>
