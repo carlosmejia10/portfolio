e320e9e227af70ca6d4b5916fbe48a1f9410754c
carlosmejia10 <117192998+carlosmejia10@users.noreply.github.com>
2023-06-25 23:55:33 -0500

    Update Skills.js

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,10 @@
-import meter1 from "../assets/img/meter1.svg";
-import meter2 from "../assets/img/meter2.svg";
-import meter3 from "../assets/img/meter3.svg";
+
 import Carousel from 'react-multi-carousel';
 import 'react-multi-carousel/lib/styles.css';
 import arrow1 from "../assets/img/arrow1.svg";
 import arrow2 from "../assets/img/arrow2.svg";
 import colorSharp from "../assets/img/color-sharp.png"
+import {ProgressBar1, ProgressBar2, ProgressBar3, ProgressBar4} from "./ProgressBar";
 
 export const Skills = () => {
   const responsive = {
@@ -30,35 +29,35 @@ export const Skills = () => {
 
   return (
     <section className="skill" id="skills">
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <div className="skill-bx wow zoomIn">
+      <div className="container">
+        <div className="row">
+          <div className="col-12">
+            <div className="skill-bx wow zoomIn">
                         <h2>Skills</h2>
-                        <p>Me especializo en el uso de herramientas para crear paginas de frontend y tambien poseo habilidades de backend. Lenguajes: React, node, SQL, HTML, CSS, javaScript.</p>
+                        <p>Me especializo en el uso de herramientas para crear paginas de frontend, sin embargo tambien poseo habilidades de backend. Lenguajes: React, node, SQL, HTML, CSS, javaScript.</p>
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
-                            <div className="item">
-                                <img src={meter1} alt="Image" />
-                                <h5>Frontend</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Backend</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter3} alt="Image" />
-                                <h5>Ux Design</h5>
-                            </div>
-                            <div className="item">
-                                <img src={meter2} alt="Image" />
-                                <h5>Databases</h5>
-                            </div>
-                        </Carousel>
-                    </div>
+                <div className="item">
+                  <ProgressBar1 percentage={65} className="progressbar1" />
+                  <h5>Frontend</h5>
                 </div>
+                <div className="item">
+                  <ProgressBar2 percentage={85} className="progressbar2" />
+                  <h5>Backed</h5>
+                </div>
+                <div className="item">
+                  <ProgressBar3 percentage={90} className="progressbar3" />
+                  <h5>Ux Design</h5>
+                </div>
+                <div className="item">
+                  <ProgressBar4 percentage={75} className="progressbar4" />
+                  <h5>Databases</h5>
+                </div>
+              </Carousel>
             </div>
+          </div>
         </div>
-        <img className="background-image-left" src={colorSharp} alt="Image" />
+      </div>
+      <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
-  )
-}
+  );
+};
